2d1863918f2c5f0e1cefd52ac75d1db524a30de3
kitatas <315kitatas10703@gmail.com>
2022-09-06 21:48:47 +0900

    add: シーン遷移

---
+++ Assets/Tameru/Scripts/View/TransitionView.cs
@@ -0,0 +1,35 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Tameru.View
+{
+    public sealed class TransitionView : MonoBehaviour
+    {
+        [SerializeField] private Image mask = default;
+
+        private readonly float fadeTime = 0.5f;
+
+        // TODO: 仮のフェード実装のため修正する
+        public async UniTask ShowAsync(CancellationToken token)
+        {
+            await mask
+                .DOFade(1.0f, fadeTime)
+                .SetEase(Ease.Linear)
+                .SetLink(gameObject)
+                .WithCancellation(token);
+        }
+
+        // TODO: 仮のフェード実装のため修正する
+        public async UniTask HideAsync(CancellationToken token)
+        {
+            await mask
+                .DOFade(0.0f, fadeTime)
+                .SetEase(Ease.Linear)
+                .SetLink(gameObject)
+                .WithCancellation(token);
+        }
+    }
+}
