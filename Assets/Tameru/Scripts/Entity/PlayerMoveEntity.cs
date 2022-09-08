56fa019c8c159cac6695ec026313f9168c91aa6d
kyoka <kyoka.ohnishi@outlook.jp>
2022-09-09 07:03:39 +0900

    remove: 使用してないメンバ変数

--- Assets/Tameru/Scripts/Entity/PlayerMoveEntity.cs
+++ Assets/Tameru/Scripts/Entity/PlayerMoveEntity.cs
@@ -13,10 +13,8 @@ namespace Tameru.Entity
     
     public class PlayerMoveEntity
     {
-        public Vector3ReactiveProperty currentMoveVec => _currentMoveVec;
         public MoveMode currentMoveMode { get; private set; }
-        private readonly Vector3ReactiveProperty _currentMoveVec;
-        
+        public Vector3 currentMoveVec { get; private set; }
 
         //MEMO: チャージ中かどうかでキャラの移動スピードが切り替わるため、enumを用いて区別する
         public void SetMoveMode(MoveMode newMoveMode)
@@ -26,7 +24,7 @@ namespace Tameru.Entity
         
         public void SetMoveVec(Vector3 newMoveVec)
         {
-            _currentMoveVec.Value = newMoveVec;
+            currentMoveVec = newMoveVec;
         }
         
         //MEMO: Animator側で「ゆっくり歩く」と「歩く」を変更してくれているので必要なし
